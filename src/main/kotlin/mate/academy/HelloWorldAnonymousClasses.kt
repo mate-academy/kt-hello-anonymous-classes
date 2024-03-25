5ba03ac63697123e1d945d4b7a7b88063e29a1bc
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-03-25 23:15:01 +0200

    Added implementation for sayHello method using anonymous classes.

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,46 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        val resultList: MutableList<String> = mutableListOf()
+        val englishGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Hello world"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Hello $someone"
+            }
+        }
+
+        val frenchGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Salut tout le monde"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Salut $someone"
+            }
+        }
+
+        val spanishGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Hola, mundo"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Hola, $someone"
+            }
+        }
+
+        resultList.add(englishGreeting.greet())
+        resultList.add(frenchGreeting.greet())
+        resultList.add(spanishGreeting.greet())
+
+        for (name in names) {
+            resultList.add(englishGreeting.greetSomeone(name))
+            resultList.add(frenchGreeting.greetSomeone(name))
+            resultList.add(spanishGreeting.greetSomeone(name))
+        }
+        return resultList.toList()
     }
 }
