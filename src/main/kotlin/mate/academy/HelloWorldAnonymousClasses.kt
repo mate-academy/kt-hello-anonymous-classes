1ba9f001d600f8f812339f0a8583b04829240c59
khorkovenko <khorkovenko>
2024-04-03 10:47:56 +0300

    done

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,34 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        val greetings = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Hello world"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Hello $someone"
+            }
+        }
+
+        val result = mutableListOf<String>()
+
+        if (names.isEmpty()) {
+            result.add(greetings.greet())
+            result.add("Salut tout le monde")
+            result.add("Hola, mundo")
+        } else {
+            result.add(greetings.greet())
+            result.add("Salut tout le monde")
+            result.add("Hola, mundo")
+
+            for (name in names) {
+                result.add(greetings.greetSomeone(name)) // Use the name as is, without trimming
+                result.add("Salut $name")
+                result.add("Hola, $name")
+            }
+        }
+
+        return result
     }
 }
