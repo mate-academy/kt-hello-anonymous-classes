63dc6c4f0a42648e5300f5f782cbf0bfad669264
Yuriy.Khilko <yurakhilko@gmail.com>
2024-05-28 12:16:56 +0300

    added solution

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,35 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        val englishGreeting = object : HelloWorldGreeting {
+            override fun greet() = "Hello world"
+
+            override fun greetSomeone(someone: String) = "Hello $someone"
+        }
+
+        val frenchGreeting = object : HelloWorldGreeting {
+            override fun greet() = "Salut tout le monde"
+
+            override fun greetSomeone(someone: String) = "Salut $someone"
+        }
+
+        val spanishGreeting = object : HelloWorldGreeting {
+            override fun greet() = "Hola, mundo"
+
+            override fun greetSomeone(someone: String) = "Hola, $someone"
+        }
+
+        val greetingList = listOf(englishGreeting, frenchGreeting, spanishGreeting)
+
+        val generalGreetings = greetingList.map { it.greet() }
+
+        val personalGreetings = names
+            .flatMap { name ->
+                greetingList
+                    .map { greeting -> greeting.greetSomeone(name) }
+            }
+
+        return generalGreetings.union(personalGreetings).toList()
+
     }
 }
