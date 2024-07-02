e89b973c7cf09a235af566a0ad0f968a8a18833e
igortrohimcuk <trokhymchuk.ihor.dev@gmail.com>
2024-07-02 20:37:46 +0300

    Solution

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -7,7 +7,29 @@ class HelloWorldAnonymousClasses {
         fun greetSomeone(someone: String): String
     }
 
+    private val greetingsList: List<HelloWorldGreeting> = listOf(
+        object : HelloWorldGreeting {
+            override fun greet() = "Hello world"
+            override fun greetSomeone(someone: String) = "Hello $someone"
+        },
+        object : HelloWorldGreeting {
+            override fun greet() = "Salut tout le monde"
+            override fun greetSomeone(someone: String) = "Salut $someone"
+        },
+        object : HelloWorldGreeting {
+            override fun greet() = "Hola, mundo"
+            override fun greetSomeone(someone: String) = "Hola, $someone"
+        }
+    )
+
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        val greetings = mutableListOf<String>()
+
+        greetingsList.forEach { greetings.add(it.greet()) }
+        for (name in names) {
+            greetingsList.forEach { greetings.add(it.greetSomeone(name)) }
+        }
+
+        return greetings
     }
 }
