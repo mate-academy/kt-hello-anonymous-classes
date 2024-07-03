5d7c663372b63b7914f05ec76dd493452b0f7476
Solomia Bobyak <mia.bobyak@gmail.com>
2024-07-03 15:27:21 +0300

    used constants and a list of anonymous classes to avoid code duplication

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -1,5 +1,13 @@
 package mate.academy
 
+const val ENGLISH_GREETING = "Hello world"
+const val FRENCH_GREETING = "Salut tout le monde"
+const val SPANISH_GREETING = "Hola, mundo"
+const val ENGLISH_GREETING_FOR_SOMEONE = "Hello %s"
+const val FRENCH_GREETING_FOR_SOMEONE = "Salut %s"
+const val SPANISH_GREETING_FOR_SOMEONE = "Hola, %s"
+
+
 class HelloWorldAnonymousClasses {
 
     interface HelloWorldGreeting {
@@ -10,46 +18,44 @@ class HelloWorldAnonymousClasses {
     fun sayHello(names: List<String>): List<String> {
         val greetingsList = mutableListOf<String>()
 
-        val englishGreeting = object: HelloWorldGreeting {
-            override fun greet(): String {
-                return "Hello world"
-            }
+        val greetings = listOf(
+            object: HelloWorldGreeting {
+                override fun greet(): String {
+                    return ENGLISH_GREETING
+                }
 
-            override fun greetSomeone(someone: String): String {
-                return "Hello $someone"
+                override fun greetSomeone(someone: String): String {
+                    return ENGLISH_GREETING_FOR_SOMEONE.format(someone)
+                }
+            },
+            object: HelloWorldGreeting {
+                override fun greet(): String {
+                    return FRENCH_GREETING
+                }
+
+                override fun greetSomeone(someone: String): String {
+                    return FRENCH_GREETING_FOR_SOMEONE.format(someone)
+                }
+            },
+            object: HelloWorldGreeting {
+                override fun greet(): String {
+                    return SPANISH_GREETING
+                }
+
+                override fun greetSomeone(someone: String): String {
+                    return SPANISH_GREETING_FOR_SOMEONE.format(someone)
+                }
             }
+        )
+
+        greetings.forEach {
+            greetingsList.add(it.greet())
         }
-
-        val frenchGreeting = object: HelloWorldGreeting {
-            override fun greet(): String {
-                return "Salut tout le monde"
-            }
-
-            override fun greetSomeone(someone: String): String {
-                return "Salut $someone"
-            }
-
-        }
-
-        val spanishGreeting = object: HelloWorldGreeting {
-            override fun greet(): String {
-                return "Hola, mundo"
-            }
-
-            override fun greetSomeone(someone: String): String {
-                return "Hola, $someone"
-            }
-
-        }
-
-        greetingsList.add(englishGreeting.greet())
-        greetingsList.add(frenchGreeting.greet())
-        greetingsList.add(spanishGreeting.greet())
 
         for (name in names) {
-            greetingsList.add(englishGreeting.greetSomeone(name))
-            greetingsList.add(frenchGreeting.greetSomeone(name))
-            greetingsList.add(spanishGreeting.greetSomeone(name))
+            greetings.forEach {
+                greetingsList.add(it.greetSomeone(name))
+            }
         }
 
         return greetingsList
