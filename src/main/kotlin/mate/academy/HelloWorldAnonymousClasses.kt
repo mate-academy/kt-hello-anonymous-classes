18a92a9611cd947c3c86d996eaf501670c857190
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2024-08-16 14:41:13 +0300

    added code

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,63 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+
+        interface HelloWorldGreeting {
+            fun greet(): String
+            fun greetSomeone(someone: String): String
+        }
+
+        fun sayHello(names: List<String>): List<String> {
+            val englishGreeting = object : HelloWorldGreeting {
+                override fun greet(): String {
+                    return "Hello world"
+                }
+
+                override fun greetSomeone(someone: String): String {
+                    return "Hello $someone"
+                }
+            }
+
+            val frenchGreeting = object : HelloWorldGreeting {
+                override fun greet(): String {
+                    return "Salut tout le monde"
+                }
+
+                override fun greetSomeone(someone: String): String {
+                    return "Salut $someone"
+                }
+            }
+
+            val spanishGreeting = object : HelloWorldGreeting {
+                override fun greet(): String {
+                    return "Hola, mundo"
+                }
+
+                override fun greetSomeone(someone: String): String {
+                    return "Hola, $someone"
+                }
+            }
+
+            val greetings = mutableListOf<String>()
+            greetings.add(englishGreeting.greet())
+            greetings.add(frenchGreeting.greet())
+            greetings.add(spanishGreeting.greet())
+
+            for (name in names) {
+                greetings.add(englishGreeting.greetSomeone(name))
+                greetings.add(frenchGreeting.greetSomeone(name))
+                greetings.add(spanishGreeting.greetSomeone(name))
+            }
+
+            return greetings
+        }
+    }
+
+    fun main() {
+        val helloWorld = HelloWorldAnonymousClasses()
+        println(helloWorld.sayHello(listOf("Alice", "Bob")))
+
+        println(helloWorld.sayHello(listOf()))
+    }
     }
 }
