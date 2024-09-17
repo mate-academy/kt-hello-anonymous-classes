2cd5dbd4a5d868b9651a5478567c8756d3a9f218
Serhii Nazarenko <nasarenko82@gmail.com>
2024-09-16 20:04:29 -0600

    first commit

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,24 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        val result: MutableList<String> = mutableListOf()
+        val greeting: HelloWorldGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Hello world, Salut tout le monde, Hola, mundo"
+            }
+
+            override fun greetSomeone(someone: String): String {
+
+                return "Hello $someone, Salut $someone, Hola, $someone"
+            }
+        }
+
+        result.addAll(greeting.greet().split(", ", limit = 3))
+
+        names.forEach { name ->
+            result.addAll(greeting.greetSomeone(name).split(", ", limit = 3))
+        }
+        return result
     }
 }
+
