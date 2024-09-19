750acfe12c9a49680da63628925601cb5c31f98c
-admin- <viktoriiamaherko@gmail.com>
2024-09-19 23:27:27 +0300

    Anonymous classes were implemented

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,40 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        val englishGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Hello world"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Hello $someone"
+            }
+        }
+
+        val frenchGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Salut tout le monde"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Salut $someone"
+            }
+        }
+
+        val spanishGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Hola, mundo"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Hola, $someone"
+            }
+        }
+        val greeters = listOf(englishGreeting, frenchGreeting, spanishGreeting)
+        val generalGreetings = greeters.map { it.greet() }
+        val personalizedGreetings = names.flatMap { name ->
+            greeters.map { it.greetSomeone(name) }
+        }
+        return generalGreetings + personalizedGreetings
     }
 }
