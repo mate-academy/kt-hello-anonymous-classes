27edc7dff6ac41ccee54a6ac34d5cd6fb869625b
Serhii Shyian <serhii.shn@gmail.com>
2025-03-26 14:22:57 +0200

    added hello-anon solution

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,38 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        val greetings = mutableListOf<String>()
+
+        // Anonymous class for English greeting
+        val englishGreeting = object : HelloWorldGreeting {
+            override fun greet(): String = "Hello world"
+            override fun greetSomeone(someone: String): String = "Hello $someone"
+        }
+
+        // Anonymous class for French greeting
+        val frenchGreeting = object : HelloWorldGreeting {
+            override fun greet(): String = "Salut tout le monde"
+            override fun greetSomeone(someone: String): String = "Salut $someone"
+        }
+
+        // Anonymous class for Spanish greeting
+        val spanishGreeting = object : HelloWorldGreeting {
+            override fun greet(): String = "Hola, mundo"
+            override fun greetSomeone(someone: String): String = "Hola, $someone"
+        }
+
+        // Add general greetings to the list
+        greetings.add(englishGreeting.greet())
+        greetings.add(frenchGreeting.greet())
+        greetings.add(spanishGreeting.greet())
+
+        // Add personalized greetings for each name
+        for (name in names) {
+            greetings.add(englishGreeting.greetSomeone(name))
+            greetings.add(frenchGreeting.greetSomeone(name))
+            greetings.add(spanishGreeting.greetSomeone(name))
+        }
+
+        return greetings
     }
 }
